291fe720b9b7042c1dc3a47f230442434638bb82
S <sheldonkorpet@gmail.com>
2023-05-05 17:31:04 +0100

    Add files via upload
    add calc functionality

--- calc.py
+++ calc.py
@@ -1,11 +1,18 @@
+# calculate the interest
+
+def calcInterest(principal, interest, time_period, frequency):
+    amount = principal * (1 + (interest / compounding_frequency)) ** (compounding_frequency * time_period)
+    interest_amount = amount - principal
+    return interest_amount
+
 # get user input
 principal = float(input("Enter the deposit amount: "))
-
 interest = float(input("Enter the interest rate: "))
+time_period = float(input("Enter the time period: "))
+compounding_frequency = float(input("Enter the compounding frequency in years: "))
 
-time_period = input("Enter the time period: ")
+# print out
+total_interest = calcInterest(principal, interest, time_period, compounding_frequency)
+total_interest = round(total_interest, 2)
 
-compounding_frequency = input("Enter the compounding frequency in years: ")
-
-# user feedback
-print(f"The initial deposit was {principal}")
+print(f"Total interest accrued is: £ {total_interest}")
